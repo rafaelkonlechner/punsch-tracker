d17c97633be1d19d638b8e24825c9fe0658517f9
Rafael Konlechner <rafael.konlechner@student.tuwien.ac.at>
2016-11-27 21:59:19 +0100

    Add bower directory to express

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
-
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static(__dirname + '/public/bower_components'));
 
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
